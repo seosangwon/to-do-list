9892c7a330faf27db7eb49292b1277538d377391
han <gksrbgks2021@gamil.com>
2023-02-07 14:49:46 +0900

    add SessionController

---
+++ src/main/java/com/example/todolist/Controller/SessionController.java
@@ -0,0 +1,10 @@
+package com.example.todolist.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SessionController {
+    @GetMapping("/")
+    public String home(){ return "redirect:/class"; }
+}
