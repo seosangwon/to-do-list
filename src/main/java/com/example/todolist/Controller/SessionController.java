8344f2c41e78c5424f47595340da856558dfa6c4
gksrbgks2021 <gksrbgks2021@gmail.com>
2023-02-10 17:23:54 +0900

    add SessionController

--- src/main/java/com/example/todolist/Controller/SessionController.java
+++ src/main/java/com/example/todolist/Controller/SessionController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SessionController {
     @GetMapping("/")
-    public String home(){ return "redirect:/class"; }
+    public String home(){ return "redirect:/login"; }
 }
