3134a23305f4c8e1ac8314e79db33cd565e9f850
gksrbgks2021 <gksrbgks2021@gmail.com>
2023-02-10 17:24:21 +0900

    add LoginRepository

---
+++ src/main/java/com/example/todolist/dao/LoginRepo.java
@@ -0,0 +1,13 @@
+package com.example.todolist.dao;
+
+import com.example.todolist.Entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+//이 클래스는 리포지토리 역할을 하고 컴포넌트 이기떄문에 컴포넌트 스캔할때 빈에 등록합니다.
+@Repository
+public interface LoginRepo extends JpaRepository<Member, String> {
+    Optional<Member> findById(String id);
+}
